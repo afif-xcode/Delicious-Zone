a13adb0dbbc3b5de2e0cde436eb43d255df0b2df
Afif Islam <islamafif1123@gmail.com>
2023-09-20 19:45:55 +0530

    Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,26 @@
-import { Routes, Route, useNavigate } from "react-router-dom"
-import Navbar from "./components/common/Navbar"
-import OpenRoute from "./components/core/Auth/OpenRoute"
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Navbar from "./components/common/Navbar";
+import OpenRoute from "./components/core/Auth/OpenRoute";
+
+
 
 // Redux
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 
 // Pages
-import Login from "./pages/Login"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+// Components
+
+// Data
 
 function App() {
   const navigate = useNavigate();
-  const dispatch = useDispatch()
-  const { user } = useSelector((state) => state.profile)
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.profile);
   return (
     <>
       <Navbar></Navbar>
@@ -34,7 +40,7 @@ function App() {
         />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,30 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: ['Poppins'],
-   },
+      sans: ["Poppins"],
+    },
     colors: {
       white: "#fff",
       black: "#000",
       transparent: "#ffffff00",
-      primaryColor : "#FF9F00",
-      secondColor : "#FFCE66",
-      backgroundColor : "#FFEFCC",
-      shadowColor : "#FFE7B3",
-      subtext : "#1F1F1F",
+      primaryColor: "#FF9F00",
+      secondColor: "#FFCE66",
+      backgroundColor: "#FFEFCC",
+      shadowColor: "#FFE7B3",
+      subtext: "#1F1F1F",
       pink: "#EF476F",
+      yellowLow: "#FCE96A",
+      yellowHigh: "#FACA15",
+      red300: "#F8B4B4",
+      yellow400: "#E3A008",
+      red400: "#F98080",
+      yellow900: "#633112",
+      yellow700: "#8E4B10",
     },
     extend: {
       maxWidth: {
         maxContent: "1260px",
-        maxContentTab: "650px"
+        maxContentTab: "650px",
       },
     },
   },
   plugins: [],
-}
+};
