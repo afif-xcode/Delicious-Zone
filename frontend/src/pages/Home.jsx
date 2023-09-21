6886758e95188d07eb7b850e235a4664c4fd96e6
Tohidujjaman Hoque <tohi7479@gmail.com>
2023-09-21 22:17:10 +0530

    Adding home styles

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import slideImage1 from "../assets/Images/featured.png"
+import slideImage2 from "../assets/Images/featured2.png"
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Navigation, Pagination } from 'swiper/modules';
 
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className='w-full bg-gradient-to-b from-yellow-75 via-yellow-100 to-primaryColor'>
+        <Swiper navigation={true} pagination={{dynamicBullets: true,}} modules={[Navigation, Pagination]} className="flex items-center justify-center" 
+              style={{"--swiper-navigation-color": "#ffff", "--swiper-navigation-size": "60px", "--swiper-pagination-color": "#fff", "--swiper-pagination-size": "60px",}}>
+          <SwiperSlide>
+            <div className='flex justify-center items-center'>
+              <img src={slideImage1} alt="" className='w-[1300px] h-[800px]' />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='flex justify-center items-center'>
+              <img src={slideImage2} alt="" className='w-[1300px] h-[750px] translate-x-16'/>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
   )
 }
 
