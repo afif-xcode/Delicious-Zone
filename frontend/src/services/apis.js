72ec10412ec5bac0f711d03113baf7ebd17a785b
Tohidujjaman Hoque <tohi7479@gmail.com>
2023-11-09 01:21:27 +0530

    Adding env file to vite

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,4 +1,5 @@
-const BASE_URL =  "http://localhost:4000/api/v1"
+const BASE_URL =  import.meta.env.VITE_BASE_URL
+console.log(BASE_URL);
 
 // CATEGORY ENDPOINTS
 export const categoryEndpoints = {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,6 @@ export default defineConfig({
     // Some libraries use the global object, even though it doesn't exist in the browser.
     // Alternatively, we could add `<script>window.global = window;</script>` to index.html.
     // https://github.com/vitejs/vite/discussions/5912
-    global: {},
+    // 'process.env': {}, 'global':{}
   },
 })
